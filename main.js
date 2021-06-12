3bc8c1dffc55d792fac9b3d70834840f72d9ddda
Niloy-Sumon <niloyabidur@gmail.com>
2021-06-13 03:30:12 +0600

    all function is working

--- main.js
+++ main.js
@@ -1,16 +1,40 @@
-const addDesposit = document.getElementById('add-deposit')
-    addDesposit.addEventListener('click', ()=>{
-        const depositCurrence = Number(document.getElementById('depositCurrence').value)
-        const depositAmount = Number(document.getElementById('depositAmount').innerText)
-        const totalDeposit = depositCurrence + depositAmount;
-        document.getElementById('depositAmount').innerText = totalDeposit;
+const addDesposit = document.getElementById("add-deposit");
+addDesposit.addEventListener("click", () => {
+  const depositCurrence = Number(document.getElementById("depositCurrence").value);
 
-        document.getElementById('depositCurrence').value = ''
+  if (depositCurrence > 0) {
+    const depositAmount = Number(document.getElementById("depositAmount").innerText);
+    const totalDeposit = depositCurrence + depositAmount;
+    document.getElementById("depositAmount").innerText = totalDeposit;
 
-        const balance = Number(document.getElementById('balance').innerText)
-        const totalBalance = totalDeposit + balance;
-        console.log(totalBalance);
-        document.getElementById('balance').innerText = totalBalance;
+    const balance = Number(document.getElementById("balance").innerText);
+    const totalBalance = totalDeposit + balance;
+    document.getElementById("balance").innerText = totalBalance;
+  }
+  
+  document.getElementById("depositCurrence").value = "";
+});
+
+
+// WithDraw btn
+const withDrawBtn = document.getElementById("withDrawBtn")
+withDrawBtn.addEventListener('click', ()=>{
+  const withDrawCurrency = Number(document.getElementById("withDrawCurrency").value);
+  if(withDrawCurrency > 0){
+  const withDrawAmount = Number(document.getElementById("withDrawAmount").innerText);
+  const total = withDrawCurrency + withDrawAmount;
+  document.getElementById("withDrawAmount").innerText = total;
+
+  const balance = Number(document.getElementById("balance").innerText)
+  const totalBalance = balance - total;
+  document.getElementById("balance").innerText = totalBalance;
+  }
+
+
+  document.getElementById("withDrawCurrency").value = ''
 })
 
 
+const sumInput = () =>{
+  
+}
